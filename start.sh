f8757ef8b119db22aa40fd7d75330fc06edd2192
Amit Jakhar <122900262+HelloUltra44@users.noreply.github.com>
2023-02-10 18:38:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y
 apt install git -y
 pip3 install -U pip
-git clone https://github.com/anumitultra/RENAMER_BOT_V3a.git          
+git clone https://github.com/HelloUltra44/renamebot          
 pip3 install -r requirements.txt
 apt install git python3-pip ffmpeg -y
 python bot.py
