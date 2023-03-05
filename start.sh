fc5e4d218d6fe974f9e6509f9d1ac289334a4711
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-03-05 21:21:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y
 apt install git -y
 pip3 install -U pip
-git clone https://github.com/HelloUltra44/Rename-Premium          
+git clone https://github.com/Apnamovi/Rename-Premium          
 pip3 install -r requirements.txt
 apt install git python3-pip ffmpeg -y
 python bot.py
